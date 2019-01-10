2145e09862b48ecb507bd374b5264b5d1a42b89d
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-10 16:32:16 -0600

    Added ability for the webpage to store the added todos locally and preserve them even when the page is refreshed

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
       task: ''
     });
   };
-  
+
   toggleComplete = (id) => {
     this.setState({todoList: this.state.todoList.map(todo => {
         if(todo.id === id) {
@@ -51,7 +51,7 @@ class App extends React.Component {
           return todo;
         }
       }) 
-    })
+    });
   };
 
   clearCompleted = e => {
@@ -63,8 +63,55 @@ class App extends React.Component {
           return null;
         }
       })
-    })
+    });
+    
   };
+
+  hydrateStateWithLocalStorage() {
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    // when user leaves/refreshes the page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+}
+componentWillUnmount() {
+  window.removeEventListener(
+    "beforeunload",
+    this.saveStateToLocalStorage.bind(this)
+  );
+
+  // saves if component has a chance to unmount
+  this.saveStateToLocalStorage();
+}
 
   render() {
     return (
